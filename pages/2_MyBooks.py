c469995904996602ead7376ea666a08dcbacfbce
christiansafelembo04-ui <christiansafelembo04@gmail.com>
2026-05-30 18:57:00 +0700

    Create 2_MyBooks.py

---
+++ pages/2_MyBooks.py
@@ -0,0 +1,28 @@
+  import streamlit as st
+
+if "cart" not in st.session_state:
+    st.session_state.cart = []
+
+if "history" not in st.session_state:
+    st.session_state.history = []
+
+st.title("📖 My Books")
+
+if not st.session_state.cart:
+    st.info("No books borrowed.")
+else:
+    for book in st.session_state.cart[:]:
+        col1,col2 = st.columns([4,1])
+
+        with col1:
+            st.write(book)
+
+        with col2:
+            if st.button("Return", key=book):
+                st.session_state.cart.remove(book)
+                st.rerun()
+
+    if st.button("Checkout"):
+        st.session_state.history.extend(st.session_state.cart)
+        st.session_state.cart.clear()
+        st.success("Checkout complete")
